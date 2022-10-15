4d0e5a0647256d8e139c2beb127f148bca4f9eb9
Filipe Poeta <filipempoeta@gmail.com>
2022-10-15 18:57:00 -0300

    Implementação de código das classes Nokia, iphone e Smartphone e teste em Program

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; }
+        private string IMEI { get; }
+        private int Memoria { get; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -21,6 +26,6 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public  abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia");
+var nokia = new Nokia(numero: "123456",modelo:" CP45X",imei:"45874412525412",memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone Iphone");
+var iphone = new Iphone(numero: "975254785",modelo:" 14 PRO MAX",imei:"100547412",memoria:256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
